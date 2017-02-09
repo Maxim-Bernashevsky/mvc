1e6b456737a97b1f789cbac728a6f4f3bf203f12
Maxim-Bernashevsky <0112358bern@mail.ru>
2017-02-09 11:04:30 +0300

    003

--- model.js
+++ model.js
@@ -1,18 +1,26 @@
 (function(){
     app.model = {
 
-        tasks: [],
-        addTask: function () {
-            var text = document.getElementById("newTask").value;
-            this.tasks.push(text);
-            app.view.showNewTask(text);
-        },
-        removeTask: function (target) {
-            //console.dir(target.children[0].innerText);
-            var targetText = target.children[0].innerText;
-            var targetNum = this.tasks.indexOf(targetText);
-            delete this.tasks[targetNum];
-            app.view.delTask(target);
+        taskFun: function () {
+            var tasks = [];
+            
+            return {
+                addTask: function () {
+                    var text = document.getElementById("newTask").value;
+                    tasks.push(text);
+                    app.view.showNewTask(text);
+                },
+                removeTask: function (target) {
+                    var targetText = target.children[0].innerText;
+                    var targetNum = tasks.indexOf(targetText);
+                    delete tasks[targetNum];
+                    app.view.delTask(target);
+                }
+            }
         }
+
+
+
+
     }
 })()
